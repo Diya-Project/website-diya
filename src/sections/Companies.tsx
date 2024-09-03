40f7704f88b67f46a0c9b5bbea6a3f5f3b716c15
ayyasyrsdy <ayyasyrsd@gmail.com>
2024-09-03 22:38:18 +0700

    fixing image talent

--- src/sections/Companies.tsx
+++ src/sections/Companies.tsx
@@ -1,4 +1,6 @@
+import Mdev from '@/assets/svg/Mdev'
 import CompanyCard from '@/templates/CompanyCard'
+import Image from 'next/image'
 import React from 'react'
 
 export default function Companies() {
@@ -6,9 +8,16 @@ export default function Companies() {
     <div className='bg-neutral-100 md:h-[50vh] p-4 flex items-center flex-col gap-5'>
         <h1 className='font-lato text-3xl font-semibold'>Talent kami tersebar di</h1>
         <div className='flex flex-wrap gap-4 w-[100%] h-[100%] justify-center'>
-            <CompanyCard borderR link='https://sharingvision.com/wp-content/uploads/2023/06/Logo-Sharingvision-Main.png'/>
-            <CompanyCard borderR link='https://mdev.co.id/wp-content/uploads/2024/06/logo_mdi_fix.svg'/>
-            <CompanyCard link='https://asaba.co.id/_next/image?url=%2Fassets%2Fimage%2Fasaba_logo_color.png&w=640&q=100'/>  
+            <CompanyCard borderR>
+                <Image src={'/assets/sharing-vision-icon.png'} alt='vision' width={400} height={400} loading='lazy'/>
+            </CompanyCard>
+            <CompanyCard borderR>
+                <Mdev/>
+            </CompanyCard>  
+            <CompanyCard>
+                <Image src={'/assets/asaba-innotech.webp'} alt='vision' width={400} height={400} loading='lazy'/>
+            </CompanyCard>  
+
         </div>
     </div>
   )
--- src/templates/CompanyCard.tsx
+++ src/templates/CompanyCard.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 type Prop={
-  link:string,
-  borderR?:boolean
+  borderR?:boolean,
+  children:ReactNode
 }
 
-export default function CompanyCard({link,borderR}:Prop) {
+export default function CompanyCard({borderR,children}:Prop) {
   return (
     <div className={`md:w-[32%] w-[100%] h-[90%] p-8 flex flex-col items-center justify-center ${borderR ? 'md:border-r md:border-b-0 border-b border-sky-700':''}`}>
-        <Image src={link} alt='mdev' width={500} height={500} loading='lazy'/>
+        {children}
     </div>
   )
 }
